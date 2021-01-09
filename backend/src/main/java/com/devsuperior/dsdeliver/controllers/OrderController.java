1df20f0a08ec461c09c9e21291bcffe31def979e
Rodrigo Queriqueli <rdqueriqueli@gmail.com>
2021-01-09 15:44:17 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
@@ -28,4 +32,17 @@ public class OrderController {
 		//retornar objeto do tipo ResponseEntity
 		return ResponseEntity.ok().body(list); //ok metodo que cria resposta que deu sucesso (code 200) 
 	}
+	
+	//endpoint para salvar - metodo http post para salvar no banco
+	//O OrderDTO dto recebido eh um JSON passado no corpo da requisicao..o framework converte o JSON num objeto java do tipo OrderDTO
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		
+		//chamada pra trazer a URI do recurso criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service  //poderia ter usado tb @Component, mas usei o service pra ficar mais semantico
 public class OrderService {
 	
 	@Autowired //notacao faz resolucao de dependencia de forma transparente
 	private OrderRepository repository;
+	
+	@Autowired //dependencia para associar produtos ao pedido
+	private ProductRepository productRepository;
 	
 	/* Caso nao usar o Autowired (Injecao de dependencia desacoplado
 	public ProductService(ProductRepository repository) {
@@ -33,4 +41,29 @@ public class OrderService {
 		//no final da linha abaixo reconverte de stream para lista
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	//retorna um OrderDTO correspondente ao pedido que inseri no banco...passando como argumento o objeto que quero salvar no banco
+	//metodo para inserir um pedido no banco, ja associado com os produtos dele
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		
+		//o objeto(OrderDTO dto) passado sera salvo no banco
+		
+		//instanciando um objeto order com esses valores, o id eh nulo pois nao eh passado pelo usuario, assim como o instante e o status que sao passados automaticos
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PEDING);
+		
+		//associar produtos que vieram dentro do objeto dto...percorrer todos ProductDTo dentro do OrderDTO passado
+		for (ProductDTO p : dto.getProducts()) {
+			//metodo getOne usado apenas pra salvar a associacao da entidade produto dentro de pedido, o produto nao eh salvo no banco, apenas a associacao
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		//orderRepository
+		//save retorno uma referencia pro objeto salvo..variavel order guarda referencia pra objeto salvo
+		order = repository.save(order);
+		//na hora de retornar retorno o objeto convertido para DTO
+		return new OrderDTO(order);
+	}
+	
 }
